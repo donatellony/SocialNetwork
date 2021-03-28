bcfe5f13b23305abd3ac5bc15449b7c4f2fb5730
donatellony <donatellony4736@gmail.com>
2021-03-28 16:45:42 +0300

    Added ngx-bootstrap and SSL certificate to the Angular side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit {
   title = 'The Social Network';
+  users: User[];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+
+  private getUsers() {
+    this.http.get<User[]>('https://localhost:5001/api/users')
+      .subscribe(users => this.users = users,
+        error => console.log(error));
+  }
+}
+
+export interface User {
+  id: number,
+  userName: string
 }
